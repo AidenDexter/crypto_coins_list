8de442c4058557c423b7027b3e3aa0ff1bc15c4c
Aiden <97030039+AidenDexter@users.noreply.github.com>
2024-03-19 16:49:09 +0400

    feat: navigator, route

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class CryptoCurrenciesListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {'/': (context) => const CryptoListScreen(), '/coin': (context) => const CryptoCoinScreen()},
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.white),
             backgroundColor: Colors.black,
             titleTextStyle: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700)),
         listTileTheme: const ListTileThemeData(iconColor: Colors.white),
@@ -33,47 +35,90 @@ class CryptoCurrenciesListApp extends StatelessWidget {
             )),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({
+    super.key,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CryptoListScreenState extends State<CryptoListScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        centerTitle: true,
+        title: const Text('CryptoCurrenciesList'),
       ),
       body: ListView.separated(
-        separatorBuilder: (context, index) => const Divider(),
-        itemCount: 10,
-        itemBuilder: (context, i) => ListTile(
-          trailing: const Icon(Icons.arrow_forward_ios),
-          leading: SvgPicture.asset(
-            'assets/icons/svg/bitcoin.svg',
-            height: 30,
-            width: 30,
-          ),
-          title: Text(
-            'CryptoCoin',
-            style: theme.textTheme.bodyMedium,
-          ),
-          subtitle: Text(
-            '20000',
-            style: theme.textTheme.labelSmall,
-          ),
-        ),
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: 10,
+          itemBuilder: (context, i) {
+            const coinName = 'Bitcoin';
+            return ListTile(
+              onTap: () {
+                Navigator.of(context).pushNamed('/coin', arguments: coinName);
+              },
+              trailing: const Icon(Icons.arrow_forward_ios),
+              leading: SvgPicture.asset(
+                'assets/icons/svg/bitcoin.svg',
+                height: 30,
+                width: 30,
+              ),
+              title: Text(
+                coinName,
+                style: theme.textTheme.bodyMedium,
+              ),
+              subtitle: Text(
+                '20000',
+                style: theme.textTheme.labelSmall,
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    assert(args != null && args is String, 'You must provide String args');
+    // if (args == null) {
+    //   print('You must provide arcs');
+    //   return;
+    // }
+
+    // if (args is! String) {
+    //   print('You must provide String args');
+    //   return;
+    // }
+    coinName = args as String;
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(coinName ?? '...'),
       ),
     );
   }
